213a368c4aa28cdfd145ad5622d8f3744be92e80
Josh <nightsphere88@gmail.com>
2018-09-04 20:42:33 -0600

    Update Navigation.js

--- client/src/components/Marginals/Navigation.js
+++ client/src/components/Marginals/Navigation.js
@@ -80,7 +80,7 @@ class Navigation extends Component {
         {sidebar}
         <Navbar className="nav_bar">
             <NavbarBrand className="nav_title" href="/">
-              t03 TripCo
+              CS 314 t03 WOPR
             </NavbarBrand>
             <div>
               {navItems.reverse()}
