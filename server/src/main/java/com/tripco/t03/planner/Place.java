ddea388c255551caf0197d5c65978199c669404e
[Lacey Willmann] <[lwillman@rams.colostate.edu]>
2018-09-05 18:04:30 -0600

    Adding the rest of Place

--- server/src/main/java/com/tripco/t03/planner/Place.java
+++ server/src/main/java/com/tripco/t03/planner/Place.java
@@ -7,52 +7,42 @@ package com.tripco.t03.planner;
 public class Place {
   public String id;
   public String name;
-  public String latitude;
-  public String longitude;
+  public double latitude;
+  public double longitude;
 
   public Place(){
-    id = null;
     name = null;
-    latitude = null;
-    longitude = null;
+    latitude = -1.0;
+    longitude = -1.0;
   }
 
-  public Place(String id, String name, String latitude, String longitude){
-    this.id = id;
+  public Place(String name, double latitude, double longitude){
     this.name = name;
     this.latitude = latitude;
     this.longitude = longitude;
-  }
-
-  public String getId(){
-    return this.id;
   }
 
   public String getName(){
     return this.name;
   }
 
-  public String getLatitude(){
+  public double getLatitude(){
     return this.latitude;
   }
 
-  public String getLongitude(){
+  public double getLongitude(){
     return this.longitude;
-  }
-
-  public void setId(String id){
-    this.id = id;
   }
 
   public void setName(String name){
     this.name = name;
   }
 
-  public void setLatitude(String latitude){
+  public void setLatitude(double latitude){
     this.latitude = latitude;
   }
 
-  public void setLongitude(String longitude){
+  public void setLongitude(double longitude){
     this.longitude = longitude;
   }
 }
--- server/src/main/java/com/tripco/t03/server/Distance.java
+++ server/src/main/java/com/tripco/t03/server/Distance.java
@@ -3,31 +3,27 @@ package com.tripco.t03.server;
 import com.google.gson.Gson;
 import com.tripco.t03.planner.Place;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
 import java.util.ArrayList;
 
 public class Distance {
     private short version = 1;
     private String type = "distance";
 
- private Place origin;
+    private Place origin;
     private Place destination;
-    private ArrayList<String> units;
-    private ArrayList<String> distance;
+    private String units;
+    private int calcDistance;
 
     public Distance(){
-        origin = null;
-        destination = null;
-        units = new ArrayList<String>();
-        distance = new ArrayList<String>();
+        origin = new Place();
+        destination = new Place();
     }
 
-    public Distance(Place orig, Place dest, ArrayList<String> units){
+    public Distance(Place orig, Place dest, String units){
         origin = orig;
         destination = dest;
         this.units = units;
-        distance = new ArrayList<String>();
+        calcDistance = -1;
     }
 
     static String getDistance() {
@@ -37,32 +33,33 @@ public class Distance {
     }
 
     private double getDeltaSigma() {
-        double deltaLongitude = Math.abs((Double.parseDouble(origin.getLongitude()) - Double.parseDouble(destination.getLongitude()))),
-                deltaLatitude = Math.abs((Double.parseDouble(origin.getLatitude()) - Double.parseDouble(destination.getLatitude()))),
-                destinationLatitude = Double.parseDouble(destination.getLatitude()),
-                originLatitude = Double.parseDouble(origin.getLatitude()),
-                destinationLongitude = Double.parseDouble(destination.getLongitude()),
-                originLongitude = Double.parseDouble(origin.getLongitude()),
-                numerator, denominator, deltaSigma;
-        
-        numerator = Math.pow((Math.cos(destinationLatitude) * Math.sin(deltaLongitude)), 2.0);
-        
-        double rightSide = Math.pow(((Math.cos(originLatitude) * Math.sin(destinationLatitude)) -
-                (Math.sin(originLatitude) * Math.cos(destinationLatitude) * Math.cos(deltaLongitude))), 2.0);
-        
-        numerator = Math.sqrt(numerator + rightSide);
+        double deltaLongitude = Math.toRadians(destination.getLongitude() - origin.getLongitude()),
+                destinationLatitude = Math.toRadians(destination.getLatitude()),
+                originLatitude = Math.toRadians(origin.getLatitude()),
+                numerator, denominator;
+
+
+        numerator = Math.sqrt(Math.pow((Math.cos(destinationLatitude) * Math.sin(deltaLongitude)), 2.0) +
+                Math.pow((Math.cos(originLatitude) * Math.sin(destinationLatitude) -
+                        Math.sin(originLatitude) * Math.cos(destinationLatitude) * Math.cos(deltaLongitude)), 2.0));
 
         denominator = Math.sin(originLatitude) * Math.sin(destinationLatitude) +
                 Math.cos(originLatitude) * Math.cos(destinationLatitude) * Math.cos(deltaLongitude);
 
-        return 1/Math.tan(numerator/denominator);
+        return Math.atan2(numerator, denominator);
     }
 
-    private void vincentyCalculation(){
-        if (u.equals("miles")) {
-            distance.add(String.format("%.2f", (3959 * getDeltaSigma())));
-        }else {
-            distance.add("-1");         
-       }
+    public void calculationDistance(){
+        switch(units) {
+            case "miles":calcDistance = (int)Math.round((3959 * getDeltaSigma())); break;
+            case "kilometers":calcDistance = (int) Math.round((6371 * getDeltaSigma())); break;
+            case "nautical miles": calcDistance = (int) Math.round((3440 * getDeltaSigma())); break;
+        }
+    }
+
+    public String toString(){
+        return String.format("Origin: latitude: %f, longitude: %f, name: %s, Destination: latitude: %f, longitude: %f, name: %s, Units: %s, Distance: %d",
+                origin.getLatitude(), origin.getLongitude(), origin.getName(), destination.getLatitude(), destination.getLongitude(), destination.getName(),
+                this.units, this.calcDistance);
     }
 }
