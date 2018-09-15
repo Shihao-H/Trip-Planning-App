b1c60ad5a782a322c1d6ca7f26053ee0aebe761b
[Lacey Willmann] <[lwillman@rams.colostate.edu]>
2018-09-14 21:27:13 -0600

    Added comments to Place and Distance Class Initial error handling Will need to be revised

--- server/src/main/java/com/tripco/t03/server/MicroServer.java
+++ server/src/main/java/com/tripco/t03/server/MicroServer.java
@@ -1,4 +1,6 @@
 package com.tripco.t03.server;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.tripco.t03.planner.Calculate;
 import com.tripco.t03.planner.Plan;
 
@@ -33,19 +35,22 @@ public class MicroServer {
     port(port);
 
     // serve the static files: index.html and bundle.js
-    Spark.staticFileLocation(this.path);
-    get("/", (req, res) -> {res.redirect("index.html"); return null;});
 
-    // register all micro-services and the function that services them.
-    // start with HTTP GET
-    get("/about", this::about);
-    get("/echo", this::echo);
-    get("/hello/:name", this::hello);
-    get("/team", this::team);
-    // client is sending data, so a HTTP POST is used instead of a GET
-    get("/config", this::config);
-    post("/plan", this::plan);
-    post("/distance", this::distance);
+    Spark.staticFileLocation(this.path);
+          get("/", (req, res) -> {
+              res.redirect("index.html");
+              return null;});
+
+          // register all micro-services and the function that services them.
+          // start with HTTP GET
+          get("/about", this::about);
+          get("/echo", this::echo);
+          get("/hello/:name", this::hello);
+          get("/team", this::team);
+          // client is sending data, so a HTTP POST is used instead of a GET
+          get("/config", this::config);
+          post("/plan", this::plan);
+          post("/distance", this::distance);
 
     System.out.println("\n\nServer running on port: " + this.port + "\n\n");
   }
@@ -57,11 +62,16 @@ public class MicroServer {
    * @return
    */
   private String about(Request request, Response response) {
-
+    String result;
     response.type("text/html");
     response.header("Access-Control-Allow-Origin", "*");
-
-    return "<html><head></head><body><h1>"+name+" Micro-server on port "+port+"</h1></body></html>";
+    try{
+      result = "<html><head></head><body><h1>"+name+" Micro-server on port "+port+"</h1></body></html>";
+    }catch(Exception e){
+      result = "{}";
+      getErrorMessage(e);
+    }
+    return result;
   }
 
   /** A REST API that returns the current server configuration
@@ -71,10 +81,16 @@ public class MicroServer {
    * @return
    */
   private String config(Request request, Response response) {
+    String result;
     response.type("application/json");
     response.header("Access-Control-Allow-Origin", "*");
-
-    return Config.getConfig();
+    try{
+      result = Config.getConfig();
+    }catch(Exception e){
+      result = "{}";
+      getErrorMessage(e);
+    }
+    return result;
   }
 
   /** A REST API that echos the client request.
@@ -84,11 +100,16 @@ public class MicroServer {
    * @return
    */
   private String echo(Request request, Response response) {
-
+    String result;
     response.type("application/json");
     response.header("Access-Control-Allow-Origin", "*");
-
-    return HTTP.echoRequest(request);
+    try{
+      result = HTTP.echoRequest(request);
+    }catch(Exception e){
+      result = "{}";
+      getErrorMessage(e);
+    }
+    return result;
   }
 
   /** A REST API demonstrating the use of a parameter.
@@ -98,11 +119,16 @@ public class MicroServer {
    * @return
    */
   private String hello(Request request, Response response) {
-
+    String result;
     response.type("text/html");
     response.header("Access-Control-Allow-Origin", "*");
-
-    return Greeting.html(request.params(":name"));
+    try{
+      result =Greeting.html(request.params(":name"));
+    }catch(Exception e){
+      result = "{}";
+      getErrorMessage(e);
+    }
+    return result;
   }
 
   /** A REST API to support trip planning.
@@ -112,11 +138,16 @@ public class MicroServer {
    * @return
    */
   private String plan(Request request, Response response) {
-
+    String result;
     response.type("application/json");
     response.header("Access-Control-Allow-Origin", "*");
-
-    return new Plan(request).getTrip();
+    try{
+      result = new Plan(request).getTrip();
+  }catch(Exception e){
+    result = "{}";
+      getErrorMessage(e);
+  }
+    return result;
   }
 
   /** A REST API that returns the team information associated with the server.
@@ -126,11 +157,16 @@ public class MicroServer {
    * @return
    */
   private String team(Request request, Response response) {
-
+    String result;
     response.type("text/plain");
     response.header("Access-Control-Allow-Origin", "*");
-
-    return name;
+    try{
+      result = name;
+    }catch(Exception e){
+      result = "{}";
+      getErrorMessage(e);
+    }
+    return result;
   }
   
     /** A REST API for distance.
@@ -140,11 +176,23 @@ public class MicroServer {
    * @return
    */
   private String distance(Request request, Response response) {
-
+    String result;
     response.type("application/json");
     response.header("Access-Control-Allow-Origin", "*");
+    try{
+      result = new Calculate(request).getDistance();
+    }catch(Exception e){
+      result = "{}";
+      getErrorMessage(e);
+    }
+    return result;
+  }
 
-    return new Calculate(request).getDistance();
+  private void getErrorMessage(Exception e){
+    System.out.println(e.getClass());
+    System.out.println(e.getCause());
+    System.out.println(e.getMessage());
+    System.out.println(e.getStackTrace());
   }
   
 }
