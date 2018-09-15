b620d514a9e88091611856da48d5de6bd4b3dc06
Lacey Willmann <31229321+lwillman@users.noreply.github.com>
2018-09-14 22:04:46 -0600

    Update Place.java

--- server/src/main/java/com/tripco/t03/planner/Place.java
+++ server/src/main/java/com/tripco/t03/planner/Place.java
@@ -5,50 +5,44 @@ package com.tripco.t03.planner;
  * There may be other attributes of a place, but these are required to plan a trip.
  */
 public class Place {
-  private String id;
-  private String name;
-  private double latitude;
-  private double longitude;
+  public String id;
+  public String name;
+  public double latitude;
+  public double longitude;
 
-  /**
-   * Default constructor
-   */
   public Place(){
     name = null;
     latitude = -1.0;
     longitude = -1.0;
   }
 
-  /**
-   * @param name name of the place string
-   * @param latitude latitude coordinates double
-   * @param longitude longitude coordinates double
-   *                  constructs an instance of Place for the desired place
-   */
   public Place(String name, double latitude, double longitude){
     this.name = name;
     this.latitude = latitude;
     this.longitude = longitude;
   }
 
-  /**
-   * @return name of place object
-   */
   public String getName(){
     return this.name;
   }
 
-  /**
-   * @return latitude of place object
-   */
   public double getLatitude(){
     return this.latitude;
   }
 
-  /**
-   * @return longitude of place object
-   */
   public double getLongitude(){
     return this.longitude;
+  }
+
+  public void setName(String name){
+    this.name = name;
+  }
+
+  public void setLatitude(double latitude){
+    this.latitude = latitude;
+  }
+
+  public void setLongitude(double longitude){
+    this.longitude = longitude;
   }
 }
