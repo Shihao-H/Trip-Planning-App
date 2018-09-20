606323517ad056f00584815c2da01f88b30a2dcb
lwillman <lwillman@rams.colostate.edu>
2018-09-19 19:37:19 -0600

    Merge branch 'master' of /s/bach/c/under/lwillman/IdeaProjects/t03 with conflicts.

--- server/src/main/java/com/tripco/t03/server/MicroServer.java
+++ server/src/main/java/com/tripco/t03/server/MicroServer.java
@@ -17,177 +17,177 @@ import static spark.Spark.*;
  */
 public class MicroServer {
 
-  private int    port;
-  private String name;
-  private String path = "/public/";
+    private int    port;
+    private String name;
+    private String path = "/public/";
 
-  /** Creates a micro-server to load static files and provide REST APIs.
-   *
-   * @param port Which port to start the server on
-   * @param name Name of the server
-   */
-  MicroServer(int port, String name) {
-    this.port = port;
-    this.name = name;
+    /** Creates a micro-server to load static files and provide REST APIs.
+     *
+     * @param port Which port to start the server on
+     * @param name Name of the server
+     */
+    MicroServer(int port, String name) {
+      this.port = port;
+      this.name = name;
 
-    port(port);
+      port(port);
 
-    // serve the static files: index.html and bundle.js
-    Spark.staticFileLocation(this.path);
-    get("/", (req, res) -> {res.redirect("index.html"); return null;});
+      // serve the static files: index.html and bundle.js
+      Spark.staticFileLocation(this.path);
+      get("/", (req, res) -> {res.redirect("index.html"); return null;});
 
-    // register all micro-services and the function that services them.
-    // start with HTTP GET
-    get("/about", this::about);
-    get("/echo", this::echo);
-    get("/hello/:name", this::hello);
-    get("/team", this::team);
-    // client is sending data, so a HTTP POST is used instead of a GET
-    get("/config", this::config);
-    post("/plan", this::plan);
-    post("/distance", this::distance);
+      // register all micro-services and the function that services them.
+      // start with HTTP GET
+      get("/about", this::about);
+      get("/echo", this::echo);
+      get("/hello/:name", this::hello);
+      get("/team", this::team);
+      // client is sending data, so a HTTP POST is used instead of a GET
+      get("/config", this::config);
+      post("/plan", this::plan);
+      post("/distance", this::distance);
 
-    System.out.println("\n\nServer running on port: " + this.port + "\n\n");
-  }
+      System.out.println("\n\nServer running on port: " + this.port + "\n\n");
+    }
 
     /** A REST API that describes the server.
-     *
-     * @param request
-     * @param response
-     * @return
-     */
-    private String about(Request request, Response response) {
-        String result;
-        response.type("text/html");
-        response.header("Access-Control-Allow-Origin", "*");
-        try{
-            result = "<html><head></head><body><h1>"+name+" Micro-server on port "+port+"</h1></body></html>";
-        }catch(Exception e){
-            result = "{}";
-            getErrorMessage(e);
-        }
-        return result;
+   *
+   * @param request
+   * @param response
+   * @return
+   */
+  private String about(Request request, Response response) {
+    String result = "";
+    response.type("text/html");
+    response.header("Access-Control-Allow-Origin", "*");
+    try{
+      result = "<html><head></head><body><h1>"+name+" Micro-server on port "+port+"</h1></body></html>";
+    }catch(Exception e){
+      result = "{}";
+      getErrorMessage(e);
     }
+    return result;
+  }
 
-    /** A REST API that returns the current server configuration
-     *
-     * @param request
-     * @param response
-     * @return
-     */
-    private String config(Request request, Response response) {
-        String result;
-        response.type("application/json");
-        response.header("Access-Control-Allow-Origin", "*");
-        try{
-            result = Config.getConfig();
-        }catch(Exception e){
-            result = "{}";
-            getErrorMessage(e);
-        }
-        return result;
+  /** A REST API that returns the current server configuration
+   *
+   * @param request
+   * @param response
+   * @return
+   */
+  private String config(Request request, Response response) {
+    String result = "";
+    response.type("application/json");
+    response.header("Access-Control-Allow-Origin", "*");
+    try{
+      result = Config.getConfig();
+    }catch(Exception e){
+      result = "{}";
+      getErrorMessage(e);
     }
+    return result;
+  }
 
-    /** A REST API that echos the client request.
-     *
-     * @param request
-     * @param response
-     * @return
-     */
-    private String echo(Request request, Response response) {
-        String result;
-        response.type("application/json");
-        response.header("Access-Control-Allow-Origin", "*");
-        try{
-            result = HTTP.echoRequest(request);
-        }catch(Exception e){
-            result = "{}";
-            getErrorMessage(e);
-        }
-        return result;
+  /** A REST API that echos the client request.
+   *
+   * @param request
+   * @param response
+   * @return
+   */
+  private String echo(Request request, Response response) {
+    String result = "";
+    response.type("application/json");
+    response.header("Access-Control-Allow-Origin", "*");
+    try{
+      result = HTTP.echoRequest(request);
+    }catch(Exception e){
+      result = "{}";
+      getErrorMessage(e);
     }
+    return result;
+  }
 
-    /** A REST API demonstrating the use of a parameter.
-     *
-     * @param request
-     * @param response
-     * @return
-     */
-    private String hello(Request request, Response response) {
-        String result;
-        response.type("text/html");
-        response.header("Access-Control-Allow-Origin", "*");
-        try{
-            result =Greeting.html(request.params(":name"));
-        }catch(Exception e){
-            result = "{}";
-            getErrorMessage(e);
-        }
-        return result;
+  /** A REST API demonstrating the use of a parameter.
+   *
+   * @param request
+   * @param response
+   * @return
+   */
+  private String hello(Request request, Response response) {
+    String result = "";
+    response.type("text/html");
+    response.header("Access-Control-Allow-Origin", "*");
+    try{
+      result =Greeting.html(request.params(":name"));
+    }catch(Exception e){
+      result = "{}";
+      getErrorMessage(e);
     }
+    return result;
+  }
 
-    /** A REST API to support trip planning.
-     *
-     * @param request
-     * @param response
-     * @return
-     */
-    private String plan(Request request, Response response) {
-        String result;
-        response.type("application/json");
-        response.header("Access-Control-Allow-Origin", "*");
-        try{
-            result = new Plan(request).getTrip();
-        }catch(Exception e){
-            result = "{}";
-            getErrorMessage(e);
-        }
-        return result;
+  /** A REST API to support trip planning.
+   *
+   * @param request
+   * @param response
+   * @return
+   */
+  private String plan(Request request, Response response) {
+    String result = "";
+    response.type("application/json");
+    response.header("Access-Control-Allow-Origin", "*");
+    try{
+      result = new Plan(request).getTrip();
+    }catch(Exception e){
+      result = "{}";
+      getErrorMessage(e);
     }
+    return result;
+  }
 
-    /** A REST API that returns the team information associated with the server.
-     *
-     * @param request
-     * @param response
-     * @return
-     */
-    private String team(Request request, Response response) {
-        String result;
-        response.type("text/plain");
-        response.header("Access-Control-Allow-Origin", "*");
-        try{
-            result = name;
-        }catch(Exception e){
-            result = "{}";
-            getErrorMessage(e);
-        }
-        return result;
+  /** A REST API that returns the team information associated with the server.
+   *
+   * @param request
+   * @param response
+   * @return
+   */
+  private String team(Request request, Response response) {
+    String result = "";
+    response.type("text/plain");
+    response.header("Access-Control-Allow-Origin", "*");
+    try{
+      result = name;
+    }catch(Exception e){
+      result = "{}";
+      getErrorMessage(e);
     }
+    return result;
+  }
 
-    /** A REST API for distance.
-     *
-     * @param request
-     * @param response
-     * @return
-     */
-    private String distance(Request request, Response response) {
-        String result;
-        response.type("application/json");
-        response.header("Access-Control-Allow-Origin", "*");
-        try{
-            result = new Calculate(request).getDistance();
-        }catch(Exception e){
-            result = "{}";
-            getErrorMessage(e);
-        }
-        return result;
+  /** A REST API for distance.
+   *
+   * @param request
+   * @param response
+   * @return
+   */
+  private String distance(Request request, Response response) {
+    String result = "";
+    response.type("application/json");
+    response.header("Access-Control-Allow-Origin", "*");
+    try{
+      result = new Calculate(request).getDistance();
+    }catch(Exception e){
+      result = "{}";
+      getErrorMessage(e);
     }
+    return result;
+  }
 
-    private void getErrorMessage(Exception e){
-        System.out.println(e.getClass());
-        System.out.println(e.getCause());
-        System.out.println(e.getMessage());
-        System.out.println(e.getStackTrace());
-    }
+  private void getErrorMessage(Exception e){
+    System.out.println(e.getClass());
+    System.out.println(e.getCause());
+    System.out.println(e.getMessage());
+    System.out.println(e.getStackTrace());
+  }
 
 }
