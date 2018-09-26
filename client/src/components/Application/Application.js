dce5942d8eab3223fd39356a4b2ba45a86422b05
Shihao-H <4amlosangeles@gmail.com>
2018-09-26 15:24:17 -0600

    try and error

--- client/src/components/Application/Application.js
+++ client/src/components/Application/Application.js
@@ -1,81 +1,95 @@
+
 import React, {Component} from 'react';
 import { Container } from 'reactstrap';
 import Info from './Info'
 import Options from './Options';
-import Upload from './Upload';
-
+import {request} from '../../api/api';
 import { get_config } from '../../api/api';
+import Upload from './Upload';
 
 /* Renders the application.
  * Holds the destinations and options state shared with the trip.
  */
 class Application extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      config: null,
-      trip: {
-        type: "trip",
-        title: "",
-        options : {
-          unit: ["miles", 'kilometers', 'nautical miles', 'user defined'], 
-          unitName: "",
-          unitRadius: 0.00
-        },
-        places: [],
-        distances: [],
-        map: '<svg width="1920" height="20" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg"><g></g></svg>'
-      }
-    };
-    this.updateTrip = this.updateTrip.bind(this);
-    this.updateBasedOnResponse = this.updateBasedOnResponse.bind(this);
-    this.updateOptions = this.updateOptions.bind(this);
-    this.updateUpload = this.updateUpload.bind(this);
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            config: null,
+            trip: {
+                type: "trip",
+                title: "",
+                options: {
+                    unit: ["miles", 'kilometers', 'nautical miles']
+                },
+                places: [],
+                distances: [],
+                map: '<svg width="1920" height="20" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg"><g></g></svg>'
+            }
+        };
+        this.updateTrip = this.updateTrip.bind(this);
+        this.updateBasedOnResponse = this.updateBasedOnResponse.bind(this);
+        this.updateOptions = this.updateOptions.bind(this);
+        this.updateUpload = this.updateUpload.bind(this);
+        this.handleSubmitHelper = this.handleSubmitHelper.bind(this);
 
-  componentWillMount() {
-    get_config().then(
-      config => {
-        this.setState({
-          config:config
-        })
-      }
-    );
-  }
+    }
+        componentWillMount()
+        {
+            get_config().then(
+                config => {
+                    this.setState({
+                        config: config
+                    })
+                }
+            );
+        }
 
-  updateTrip(field, value) {
-    let trip = this.state.trip;
-    trip[field] = value;
-    this.setState(trip);
-  }
+        updateTrip(field, value)
+        {
+            let trip = this.state.trip;
+            trip[field] = value;
+            this.setState(trip);
+        }
 
-  updateBasedOnResponse(value) {
-    this.setState({'trip': value});
-  }
+        updateBasedOnResponse(value)
+        {
+            this.setState({'trip': value});
+        }
 
-  updateOptions(option, value){
-    let trip = this.state.trip;
-    trip.options[option] = value;
-    this.setState(trip);
-  }
+        updateOptions(option, value)
+        {
+            let trip = this.state.trip;
+            trip.options[option] = value;
+            this.setState(trip);
+        }
 
-  updateUpload(option, value){
-    let trip = this.state.trip;
-    trip.upload[option] = value;
-    this.setState(trip);
-  }
+        updateUpload(option, value)
+        {
+            let trip = this.state.trip;
+            trip.upload[option] = value;
+            this.setState(trip);
+        }
 
-  render() {
-    if(!this.state.config) { return <div/> }
+        handleSubmitHelper()
+        {
+            let trip = this.state.trip;
+            return trip;
+        }
 
-    return(
-      <Container id="Application">
-        <Info/>
-        <Options options={this.state.trip.options} config={this.state.config} updateOptions={this.updateOptions}/>
-          <Upload upload={this.state.trip.upload} config={this.state.config} updateUpload={this.updateUpload}/>
-      </Container>
-    )
-  }
+        render()
+        {
+            if (!this.state.config) {
+                return <div/>
+            }
+            return (
+                <Container id="Application">
+                    <Info/>
+                    <Upload file={this.state.trip} config={this.state.config} updateTrip={this.updateTrip}/>
+                    <Options options={this.state.trip.options} config={this.state.config}
+                             updateOptions={this.updateOptions}/>
+                </Container>
+            )
+        }
+
 }
-
-export default Application;
+    export default Application;
--- client/src/components/Application/Upload.js
+++ client/src/components/Application/Upload.js
@@ -1,38 +1,57 @@
 import React, {Component} from 'react'
 import {Button} from "reactstrap";
 import { Card, CardHeader, CardBody } from 'reactstrap'
+import { request } from '../../api/api';
+
 //import ReactDOM from 'react-dom'
-
-
-
-class Upload extends Component{
+class Upload extends Component {
     constructor(props) {
         super(props);
         this.handleSubmit = this.handleSubmit.bind(this);
         this.fileInput = React.createRef();
     }
+
+    LoadFile(event) {
+        let file=event.target.files[0];
+        let fileReader = new FileReader();;
+        fileReader.readAsText(file)
+        fileReader.onload = (event) =>{
+            console.log("file data", event.target.result);
+            let obj=JSON.parse(event.target.result);
+            console.log("Try and error ",obj);
+        }
+    }
+
     handleSubmit(event) {
-        event.preventDefault();
+        // event.preventDefault();
         alert(
             `Selected file - ${
                 this.fileInput.current.files[0].name
-                }`
-        );
+                }`);
+        let obj=this.props.handleSubmitHelper();
+        // console.log(typeof this.props.state.trip);
+        request(obj,'plan').then((FinalTrip)=>
+        {
+            console.log(typeof FinalTrip);
+            console.log(FinalTrip);
+        });
+        // this.props.updateTrip()
     }
 
     render() {
-        // highlight-range{5}
         return (
             <div className="card">
                 <CardBody>
                     <form onSubmit={this.handleSubmit}>
                         <label>
                             Submit your trip!
-                            <br /><br />
-                            <input type="file" ref={this.fileInput} />
-                            <small className="form-text text-muted">Upload a file that contains a Trip object in the TFFI format.</small>
+                            <br/><br/>
+                            <input type="file" ref={this.fileInput} onChange={(event)=>this.props.updateTrip("file",this.LoadFile(event))}/>
+                            <small className="form-text text-muted">
+                                Upload a file that contains a Trip object in the TFFI format.
+                            </small>
                         </label>
-                        <br />
+                        <br/>
                         <Button className='btn-outline-dark' type="submit">Submit</Button>
                     </form>
                 </CardBody>
@@ -40,7 +59,6 @@ class Upload extends Component{
         );
     }
 
-//ReactDOM.render(<FileInput />,document.getElementById('root'));
- }
+}
 
 export default Upload;
