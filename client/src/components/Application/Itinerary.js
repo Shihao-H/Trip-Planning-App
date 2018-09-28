1370a0afd6212f11449e458fcb5bf2a8b309f78f
MinjieSh <42553263+MinjieSh@users.noreply.github.com>
2018-09-27 20:02:14 -0600

    Add total distances

--- client/src/components/Application/Itinerary.js
+++ client/src/components/Application/Itinerary.js
@@ -10,39 +10,48 @@ class Itinerary extends Component {
     createTable(){
         let table = [];
         let children = [];
-
+        let total_distance = 0;
         if(this.props.trip.places.length === 0){
-            children.push(<th key='origin'>{"Origin City"}</th>);
-            children.push(<th key='destination'>{"Destination City"}</th>);
-            children.push(<th key='distance'>{"Distance"}</th>);
-            table.push(<tr key='header'>{children}</tr>);
+            children.push(<th key='origin'>{"Origin city"}</th>);
+            children.push(<th key='destination'>{"Destination city"}</th>);
+            children.push(<th key='leg distance'>{"Leg distance"}</th>);
+            children.push(<th key='total distance'>{"Total distance"}</th>);
+            table.push(<tr key='default header'>{children}</tr>);
             return table
         } else {
-            children.push(<th key='origin'>{"Origin City"}</th>);
-            children.push(<th key='destination'>{"Destination City"}</th>);
-            children.push(<th key='distance'>{"Distance"}</th>);
+            children.push(<th key='origin'>{"Origin city"}</th>);
+            children.push(<th key='destination'>{"Destination city"}</th>);
+            children.push(<th key='leg distance'>{"Leg distance"}</th>);
+            children.push(<th key='total distance'>{"Total distance"}</th>);
             table.push(<tr key='first row'>{children}</tr>);
             let cell = [];
             for (let i = 0; i < this.props.trip.places.length - 1; i++) {
                 cell = [];
                 cell.push(<th key={'origin' + i}>{this.props.trip.places[i].name}</th>);
                 cell.push(<th key={'destination' + i}>{this.props.trip.places[i + 1].name}</th>);
-                //console.log(typeof this.props.trip.distances);
+
                 if (this.props.trip.distances.length === 0) {
-                    cell.push(<th key={'distance' + i}>{'0'}</th>);
+                    cell.push(<th key={'leg distance' + i}>{'0'}</th>);
+                    cell.push(<th key={'total distance' + i}>{'0'}</th>);
                 } else {
-                    cell.push(<th key={'distance' + i}>{this.props.trip.distances[i]}</th>);
+                    cell.push(<th key={'leg distance' + i}>{this.props.trip.distances[i]}</th>);
+                    total_distance = total_distance + this.props.trip.distances[i];
+                    cell.push(<th key={'total distance' + i}>{total_distance}</th>);
                 }
                 table.push(<tr key={'row' + i}>{cell}</tr>)
             }
+
             cell=[];
             cell.push(<th key="origin last">{this.props.trip.places[this.props.trip.places.length - 1].name}</th>);
             cell.push(<th key="destination last">{this.props.trip.places[0].name}</th>);
 
             if (this.props.trip.distances.length === 0) {
-                cell.push(<th key={'distance last'}>{'0'}</th>);
+                cell.push(<th key={'last leg distance'}>{'0'}</th>);
+                cell.push(<th key={'total distance'}>{'0'}</th>);
             } else {
-                cell.push(<th key="distance last">{this.props.trip.distances[this.props.trip.places.length - 1]}</th>);
+                cell.push(<th key="last leg distance">{this.props.trip.distances[this.props.trip.places.length - 1]}</th>);
+                total_distance = total_distance + this.props.trip.distances[this.props.trip.places.length - 1];
+                cell.push(<th key={'total distance'}>{total_distance}</th>);
             }
             table.push(<tr key={'last row'}>{cell}</tr>);
             return table;
