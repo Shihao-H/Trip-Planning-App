0b599d0ab364b9a485629fa9c4c8219ca86e41f6
Lacey Willmann <lwillman@rams.colostate.edu>
2018-09-29 01:57:48 -0600

    Add LineDistance.java Add TestLineDistance.java

--- server/src/main/java/com/tripco/t03/planner/LineDistance.java
+++ server/src/main/java/com/tripco/t03/planner/LineDistance.java
@@ -6,73 +6,43 @@ public class LineDistance {
     public ArrayList<Place> places;
     private double centerLat = 39.0;
     private double centerLong = -105.5472;
-    private double latToPxlEast = 177.08811;
-    private double latToPxlWest = 177.16875;
-    private double longToPxlNorth = 141.6839;
-    private double longToPxlSouth = 142.0528;
-    private boolean north;
-    private boolean east;
+
 
     public LineDistance(ArrayList<Place> places){
         this.places = places;
     }
 
-    private void setLatQuad(double lat, double longitude){
-        if (lat > centerLat){
-            this.north = true;
+    private boolean setNorth(double lat) {
+        return lat > centerLat;
+    }
+
+    private boolean setEast(double longitude){
+        return longitude > centerLong;
+    }
+
+    private double getLatDistFromCenter(double lat, boolean east){
+        double distFromCent = lat - centerLat;
+        if(east) {
+            return distFromCent * 177.08811;
         }else{
-            this.north = false;
-        }
-        if (longitude > centerLong){
-            this.east = true;
-        }
-        else{
-            this.east = false;
+            return distFromCent*177.16875;
         }
     }
 
-    private double getLatDistFromCenter(double lat){
-        double distFromCent;
-        if(north){
-            distFromCent = lat - centerLat;
-        }else{
-            distFromCent = centerLat-lat;
-        }
-        if(east) {
-            return distFromCent * latToPxlEast;
-        }else{
-            return distFromCent*latToPxlWest;
-        }
-    }
-
-    private double getLongDistFromCenter(double longitude){
-        double distFromCent;
-        if(east) {
-            distFromCent = longitude-centerLong;
-        }else{
-            distFromCent = centerLong-longitude;
-        }
+    private double getLongDistFromCenter(double longitude, boolean north){
+        double distFromCent = longitude-centerLong;
         if (north) {
-            return distFromCent * longToPxlNorth;
+            return distFromCent * 141.6839;
         } else {
-            return distFromCent * longToPxlSouth;
+            return distFromCent * 142.0528;
         }
     }
 
-    public ArrayList<Double> getCoordinates(ArrayList<Place> places){
+    public ArrayList<Double> getCoordinates(){
         ArrayList<Double> coordinates = new ArrayList<>();
-        for(int i = 0; i < places.size(); i++) {
-            setLatQuad(places.get(i).latitude, places.get(i).longitude);
-            if (north) {
-                coordinates.add(391.5412 - getLatDistFromCenter(places.get(i).latitude));
-            } else {
-                coordinates.add(391.5412 + getLatDistFromCenter(places.get(i).latitude));
-            }
-            if (east) {
-                coordinates.add(30365 + getLongDistFromCenter(places.get(i).longitude));
-            } else {
-                coordinates.add(533.30365 - getLongDistFromCenter(places.get(i).longitude));
-            }
+        for(int i = 0; i < this.places.size(); i++) {
+            coordinates.add(533.30365 + getLongDistFromCenter(this.places.get(i).longitude, setNorth(places.get(i).latitude)));
+            coordinates.add(391.5412 - getLatDistFromCenter(this.places.get(i).latitude, setEast(places.get(i).longitude)));
         }
         return coordinates;
     }
---
+++ server/src/test/java/com/tripco/t03/planner/TestLineDistance.java
@@ -0,0 +1,57 @@
+package com.tripco.t03.planner;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import java.util.ArrayList;
+
+public class TestLineDistance {
+
+    private LineDistance ld;
+    private ArrayList<Place> places;
+    private ArrayList<Double> expectedXY;
+    @Before
+    public void setUp(){
+        places = new ArrayList<>();
+        expectedXY = new ArrayList<>();
+        places.add(new Place("orig", "Montrose", 38.478321, -107.876175));
+        places.add(new Place("dest1", "Durango ", 37.270500, -107.878700));
+        places.add(new Place("dest2", "Denver ", 39.742043, -104.991531));
+        places.add(new Place("dest3", "Fairplay ", 39.2247125, -106.00196180));
+        places.add(new Place("dest4", "Fort Collins ", 40.585258, -105.084419));
+        places.add(new Place("dest5", "Four Corners ", 36.9927152491, -109.040));
+        places.add(new Place("dest6", "Pueblo", 38.276463, -104.604607));
+        places.add(new Place("dest7", "3 Corners CO/UT/WY ", 41.000659, -109.050075));
+        expectedXY.add(202.466230); expectedXY.add(483.966416); expectedXY.add(202.107547); expectedXY.add(697.954553);
+        expectedXY.add(612.033001); expectedXY.add(260.134208); expectedXY.add(468.871225); expectedXY.add(351.729167);
+        expectedXY.add(598.872267); expectedXY.add(110.810857); expectedXY.add(37.141630); expectedXY.add(747.169330);
+        expectedXY.add(667.201625); expectedXY.add(519.671000); expectedXY.add(37.002659); expectedXY.add(37.086158);
+    }
+
+    @Test
+    public void testLD(){
+        ld = new LineDistance(places);
+
+        assertEquals(places, ld.places);
+    }
+
+    private boolean equals(ArrayList<Double> arr2){
+        for(int i = 0; i < this.expectedXY.size(); i++) {
+            if (Math.abs(this.expectedXY.get(i) - arr2.get(i)) > 1.0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Test
+    public void testGetXY(){
+        ld = new LineDistance(places);
+        ArrayList<Double> result = ld.getCoordinates();
+
+        System.out.println(result + " , \n\n" + expectedXY);
+
+        Assert.assertTrue(equals(result));
+    }
+}
