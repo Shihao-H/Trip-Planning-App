343446174ac4154bec8cf72a35535655a58d5840
Shihao-H <4amlosangeles@gmail.com>
2018-10-05 00:25:22 -0600

    Dropdown for Map

--- client/src/components/Application/Map.js
+++ client/src/components/Application/Map.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardText, CardBody,
-    CardTitle, CardSubtitle, Button } from 'reactstrap';
+import {Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button, Collapse} from 'reactstrap';
 import colorado from '../../../../images/CObackground.svg';
 
 
 
 export class Map extends Component {
+    constructor(props)
+    {
+        super(props);
+        this.state = {
+            collapse: true,
+        };
+        this.dropdown = this.dropdown.bind(this);
+    }
+    dropdown()
+    {
+        this.setState({collapse: !this.state.collapse})
+    }
+
     render() {
         return (
             <div>
-                <Card>
-                    <CardImg  width="75%" src={colorado} alt={"Map of Colorado"}/>
-                </Card>
-            </div>)
+                <Button onClick={this.dropdown} className = 'btn-dark' size='lg'>Map</Button>
+                    <Collapse isOpen = {this.state.collapse}>
+                        <Card>
+                            <CardImg  width="75%" src={colorado} alt={"Map of Colorado"}/>
+                        </Card>
+                    </Collapse>
+            </div>);
     }
 }
