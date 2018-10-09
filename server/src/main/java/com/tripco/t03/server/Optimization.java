ac76e7855ae1e2c262cfb851419157121620b071
Josh <nightsphere88@gmail.com>
2018-10-09 14:32:35 -0600

    Updated config response

---
+++ server/src/main/java/com/tripco/t03/server/Optimization.java
@@ -0,0 +1,17 @@
+package com.tripco.t03.server;
+
+public class Optimization {
+
+    public String label;
+    public String description;
+
+    public Optimization() {
+        label = null;
+        description = null;
+    }
+
+    public Optimization(String lab, String desc) {
+        this.label = lab;
+        this.description = desc;
+    }
+}
