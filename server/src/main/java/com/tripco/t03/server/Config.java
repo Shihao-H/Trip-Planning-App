366264a3d0be05922cef49458c4f271b2ecec1f9
Lacey Willmann <lwillman@rams.colostate.edu>
2018-10-12 19:25:02 -0600

    Cleaned up spacing in Config.java

--- server/src/main/java/com/tripco/t03/server/Config.java
+++ server/src/main/java/com/tripco/t03/server/Config.java
@@ -1,7 +1,6 @@
 package com.tripco.t03.server;
 
 import com.google.gson.Gson;
-
 import java.util.Arrays;
 import java.util.List;
 
@@ -15,13 +14,11 @@ public class Config {
   public Config(){
     units = Arrays.asList("miles", "kilometers", "nautical miles", "user defined");
     optimization = Arrays.asList(new Optimization("none", "The trip is not optimized."), new Optimization("short", "Nearest neighbor."));
-
   }
 
   static String getConfig() {
     Config conf = new Config();
     Gson gson = new Gson();
-
     return gson.toJson(conf);
   }
 }
