77d05010dc8851851e9f1fed4a62d14b039baf10
MinjieSh <42553263+minjiesh@users.noreply.github.com>
2018-10-17 11:58:18 -0600

    * enable add and remove destinations * enable reverse the order of the trip * enable select attributes to display in the itinerary (include latitude and longitude)

--- client/src/components/Application/Upload.js
+++ client/src/components/Application/Upload.js
@@ -1,14 +1,9 @@
 import React, {Component} from 'react'
-import {Button} from "reactstrap";
+import {Button, Row, Col, Container} from "reactstrap";
 import {CardBody} from 'reactstrap'
 import {request} from '../../api/api';
-import SaveTrip from './SaveTrip';
 
 class Upload extends Component {
-    /* This is a constructor for upload.
-     * otherTeams: Users are supposed to give a port number from other teams as input,
-     * in order to enable clients and servers to inter-operate.
-     */
     constructor(props) {
         super(props);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -18,19 +13,11 @@ class Upload extends Component {
         this.state = {otherTeams: null};
     }
 
-    /* This function updates the value of otherTeam (a port number from other teams).
-     * A event is a user typing something in the input text field.
-     */
     updateOtherTeams(event) {
         this.setState({otherTeams: event.target.value});
 
     }
 
-    /* This function loads the file using a fileReader.
-     * This function will be called if we click the load button.
-     * Parse the file to a JSON object and then update the trip object (in application).
-     * This is related to lifting states up.
-     */
     LoadFile(event) {
         let file=event.target.files[0];
         let fileReader = new FileReader();
@@ -47,16 +34,11 @@ class Upload extends Component {
             }
             this.props.updateTrip('places', obj.places);
             this.props.updateTrip('map', obj.map);
-
         }
-
+        this.props.updateSelected(new Map());
+        this.props.updateSelectAll(false);
     }
-    
-    /* This function plans the trip which means it calculates the distances from places.
-     * This function will be called if we click the plan button.
-     * By default, it runs on our server (localhost 8088/ localhost 31403/ black-bottle 31403).
-     * If someone types in a port number from other teams, it will run on that server in order to achieve inter-operate.
-     */
+
     handleSubmit(event) {
         event.preventDefault();
         let obj=this.props.trip;
@@ -65,45 +47,40 @@ class Upload extends Component {
             {
                 this.props.updateTrip('distances',Fi.distances);
             });
-       } else {
+        } else {
             request(obj,'plan', this.state.otherTeams, 'black-bottle.cs.colostate.edu').then((Fi)=>
             {
-                console.log('g',this.state.otherTeams);
                 this.props.updateTrip('distances',Fi.distances);
             });
         }
     }
 
-    /* Renders the upload component.
-     * Holds sate of everything we get from the file shared with the trip.
-     */
     render() {
         return (
             <div className="card">
                 <CardBody>
-                    <form>
-                        <label>
-                            Submit your trip!
+                    <Row>
+                        <Col md={6}>
+                            <label>
+                                Submit your trip!
+                                <br/><br/>
+                                <input type="file" ref={this.fileInput} onChange={this.LoadFile}/>
+                                <small className="form-text text-muted">
+                                    Upload a file that contains a Trip object in the TFFI format.
+                                </small>
+                            </label>
+                            <br/>
+                            <Button className='btn-outline-dark' size='lg' type="submit" onClick={this.handleSubmit}>Plan</Button>
                             <br/><br/>
-                            <input type="file" ref={this.fileInput} onChange={this.LoadFile}/>
-                            <small className="form-text text-muted">
-                                Upload a file that contains a Trip object in the TFFI format.
-                            </small>
-                        </label>
-                        <br/>
-                        <Button className='btn-outline-dark' size='lg' type="submit" onClick={this.handleSubmit}>Plan</Button>
-                        <br/><br/>
-                        REST API Server Address Port:
-                        <br/>
-                        <input type="text"
-                               placeholder="for example: 31403"
-                               style={{width: 300}}
-                               onChange={this.updateOtherTeams}
-                        />
-                        <br/>
-                            <SaveTrip/>
-                        <br/>
-                    </form>
+                            REST API Server Address Port:
+                            <br/>
+                            <input type="text"
+                                   placeholder="for example: 31403"
+                                   style={{width: 300}}
+                                   onChange={this.updateOtherTeams}
+                            />
+                        </Col>
+                    </Row>
                 </CardBody>
             </div>
         );
