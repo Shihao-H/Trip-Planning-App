0af50763d63036c2641cf813bff185fdd5339784
Lacey Willmann <31229321+lwillman@users.noreply.github.com>
2018-10-27 19:00:15 -0600

    Updated DistanceGrid
    Made changes recommended by codeclimate

--- server/src/main/java/com/tripco/t03/planner/DistanceGrid.java
+++ server/src/main/java/com/tripco/t03/planner/DistanceGrid.java
@@ -8,16 +8,31 @@ public class DistanceGrid {
     private String userDefinedUnitName;
     private Double userDefinedRadius;
 
+    /**
+    *Default contructor.
+    */
     public DistanceGrid(){
         this.distanceGrid = null;
     }
 
+    /**
+    *Constructor for non user defined options
+    *@param location array of Places.
+    *@param units string of unit type.
+    */
     public DistanceGrid(Place[] location, String units){
         this.locations=location;
         this.units = units;
         this.distanceGrid = new Distance[locations.length][locations.length];
     }
 
+    /**
+    *Constructor for userd defined options
+    *@param locations array of Places.
+    *@param units String unit type.
+    *@param udUnitName String name of user defined unit.
+    *@param udRadius Double the radius of Earth in user defined units.
+    */
     public DistanceGrid(Place[] locations, String units, String udUnitName, Double udRadius){
         this.locations = locations;
         this.units = units;
@@ -26,6 +41,9 @@ public class DistanceGrid {
         this.distanceGrid = new Distance[locations.length][locations.length];
     }
 
+    /**
+    *Builds a 2D array of Distance objects.
+    */
     public void buildGrid(){
         for (int i = 0; i < this.locations.length; i++) {
             distanceGrid[i][i] = null;
@@ -35,13 +53,11 @@ public class DistanceGrid {
                 if (distanceGrid[i][j] == null) {
                     if (this.units.equalsIgnoreCase("user defined")) {
                         distanceGrid[i][j] = new Distance(this.locations[i], locations[j], this.units, this.userDefinedUnitName, this.userDefinedRadius);
-                        distanceGrid[i][j].setDistance();
-                        distanceGrid[j][i] = new Distance(locations[j], locations[i], this.units, distanceGrid[i][j].distance);
                     } else {
                         distanceGrid[i][j] = new Distance(locations[i], locations[j], this.units);
-                        distanceGrid[i][j].setDistance();
-                        distanceGrid[j][i] = new Distance(locations[j], locations[i], this.units, distanceGrid[i][j].distance);
                     }
+                    distanceGrid[i][j].setDistance();
+                    distanceGrid[j][i] = new Distance(locations[j], locations[i], this.units, distanceGrid[i][j].distance);
                 }
             }
         }
