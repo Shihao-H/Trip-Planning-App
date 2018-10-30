e9166aa1d3f35d1d1b084bf2904a0038080cbb68
Lacey Willmann <lwillman@rams.colostate.edu>
2018-10-30 17:03:31 -0600

    Updated MergeSort for new optimization Updated TestMergeSort

--- server/src/test/java/com/tripco/t03/planner/TestMergeSortPlace.java
+++ server/src/test/java/com/tripco/t03/planner/TestMergeSortPlace.java
@@ -4,24 +4,25 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 public class TestMergeSortPlace {
-    private Place[] places;
-    private Place[] expected;
+    private ArrayList<Place> places = new ArrayList<>();
+
     @Before
     public void setup(){
-        places = expected = new Place[6];
-        places[0] = expected[4] = new Place("01", "one", 20.0000, 99.33333);
-        places[1] = expected[3] = new Place("02", "two", 20.0000, 25.99999);
-        places[2] = expected[5] = new Place("03", "three", 20.0000, 140.66666);
-        places[3] = expected[0] = new Place("04", "four", 20.0000, -110.77777);
-        places[4] = expected[2] = new Place("05", "five", 20.0000, 2.555555);
-        places[5] = expected[1] = new Place("06", "six", 20.0000, -0.55555);
+        places.add(new Place("01", "one", 20.0000, 99.33333));
+        places.add(new Place("02", "two", 20.0000, 25.99999));
+        places.add(new Place("03", "three", 20.0000, 140.66666));
+        places.add(new Place("04", "four", 20.0000, -110.77777));
+        places.add(new Place("05", "five", 20.0000, 2.555555));
+        places.add(new Place("06", "six", 20.0000, -0.55555));
     }
 
     @Test
     public void testMergeSort(){
-        Place [] result = MergeSortPlace.sort( places);
+        Integer[] result = MergeSortPlace.sort( places);
 
-        Assert.assertEquals(expected[0], result[0]);
+        Assert.assertEquals(places.size(), result.length);
     }
 }
