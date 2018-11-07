ff3e0d5a1e15c5d193e9c7694ae2bd7cc1d06e33
MinjieSh <42553263+MinjieSh@users.noreply.github.com>
2018-11-07 01:29:51 -0700

    Update OptionPanel.js

--- client/src/components/Application/OptionPanel.js
+++ client/src/components/Application/OptionPanel.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Container,Row,Col,Card,CardBody} from 'reactstrap';
+import {Container, Row, Col, Card, CardBody} from 'reactstrap';
 import Attributes from "./Attributes"
 import InterOperate from "./InterOperate"
 import Optimization from "./Optimization";
@@ -10,7 +10,7 @@ import Trip from "./Trip";
  * Holds Clear, Load, Plan, Save.
  */
 class OptionPanel extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
     }
 
@@ -20,29 +20,28 @@ class OptionPanel extends Component {
                 <CardBody><p className="lead">Options</p></CardBody>
                 <Container id="Plan">
                     <Row><Col>
-                            <Options config={this.props.config} options={this.props.options}
-                            updateOptions={this.props.updateOptions}/>
-                        </Col>
+                        <Options config={this.props.config} options={this.props.options}
+                                 updateOptions={this.props.updateOptions}/>
+                    </Col>
                         <Col>
                             <Optimization config={this.props.config} options={this.props.options}
-                            search={this.props.search} trip={this.props.trip}
-                            updateOptions={this.props.updateOptions} updateSearch={this.props.updateSearch}/>
+                                          search={this.props.search} trip={this.props.trip}
+                                          updateOptions={this.props.updateOptions}
+                                          updateSearch={this.props.updateSearch}/>
                         </Col>
                         <Col>
-                            <Attributes config={this.props.config} trip={this.props.trip}
-                                        attributes={this.props.attributes}
-                                        display={this.props.display} updateDisplay={this.props.updateDisplay}
-                                        updateAttributes={this.props.updateAttributes}
-                                        checkAttributes={this.props.checkAttributes}
-                                        updateDisplayUserDefined={this.props.updateDisplayUserDefined}/>
+                            <Attributes config={this.props.config} display={this.props.display}
+                                        updateAttributes={this.props.updateAttributes}/>
                         </Col>
                         <Col>
                             <InterOperate host={this.props.host} otherTeams={this.props.otherTeams}
-                                updateHost={this.props.updateHost} updateOtherTeams={this.props.updateOtherTeams}/>
+                                          updateHost={this.props.updateHost}
+                                          updateOtherTeams={this.props.updateOtherTeams}/>
                         </Col></Row>
                 </Container>
             </Card>
         )
     }
 }
+
 export default OptionPanel;
