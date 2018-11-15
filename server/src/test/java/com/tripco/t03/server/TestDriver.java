942318e1e6369ab1c73c705ce9a7e1053e6b5cc6
MinjieSh <42553263+minjiesh@users.noreply.github.com>
2018-11-14 21:52:08 -0700

    * test coverage 89%

--- server/src/test/java/com/tripco/t03/server/TestDriver.java
+++ server/src/test/java/com/tripco/t03/server/TestDriver.java
@@ -1,41 +1,72 @@
 package com.tripco.t03.server;
 
-import org.junit.Assert;
+import com.tripco.t03.planner.Place;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 
-import java.sql.ResultSet;
+import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
 
 public class TestDriver {
-    private Driver driver;
     private Driver testDriver;
     private String match;
     private int limit;
     private String filterQuery;
     private String limitQuery;
+    private ArrayList<Place> expected;
 
     @Before
     public void setup(){
         match = "alf";
-        limit = 20;
-        filterQuery = "AND continents.name IN (\"Asia\", \"North America\")\n"
-                + "AND type IN (\"heliport\", \"large_airport\")\n";
+        filterQuery = "AND continents.name IN (\"North America\")\n"
+                + "AND type IN (\"heliport\")\n";
         limitQuery = "limit 20";
-        Driver driver = new Driver();
-        driver.find(match, limit, filterQuery);
+        limit = 20;
+        expected = new ArrayList<>();
+        expected.add(new Place("0FL4", "Jackson Heliport", 30.705, -85.378304));
+        expected.add(new Place("98TX", "Cig 402 Heliport", 27.256701, -98.0942));
+        expected.add(new Place("US-VA38", "Lz Alfa Heliport", 36.793201, -75.964699));
     }
 
     @Test
     public void testFind(){
-        ResultSet
         testDriver = new Driver();
-        driver.setSearch(match, filterQuery);
-        assertEquals(testDriver.search, expected);
-
+        int found = 3;
+        testDriver.find(match, limit, filterQuery);
+        assertEquals(testDriver.places.size(), expected.size());
+        assertEquals(testDriver.found, found);
+        assertEquals(testDriver.limitQuery, limitQuery);
     }
 
+    @Test
+    public void testFindWithZeroLimit(){
+        limit = 0;
+        limitQuery = "";
+        testDriver = new Driver();
+        int found = 3;
+        testDriver.find(match, limit, filterQuery);
+        assertEquals(testDriver.places.size(), expected.size());
+        assertEquals(testDriver.found, found);
+        assertEquals(testDriver.limitQuery, limitQuery);
+    }
+
+    @Rule
+    public final SystemErrRule systemErrRule = new SystemErrRule().enableLog();
+
+    @Test
+    public void testFindException(){
+        testDriver = new Driver();
+        boolean thrown = false;
+        filterQuery = "AND continents IN (\"North America\")\n"
+                + "AND type IN (\"heliport\")\n";
+        System.err.println("Exception: Unknown column 'continents' in 'where clause'");
+
+        testDriver.find(match, limit, filterQuery);
+        assertEquals("Exception: Unknown column 'continents' in 'where clause'", systemErrRule.getLog());
+
+    }
 
     @Test
     public void testSetSearch(){
@@ -59,7 +90,7 @@ public class TestDriver {
                 + "world_airports.municipality, world_airports.name ASC "
                 + limitQuery;
         testDriver = new Driver();
-        driver.setSearch(match, filterQuery);
+        testDriver.setSearch(match, filterQuery);
         assertEquals(testDriver.search, expected);
     }
 
@@ -80,15 +111,7 @@ public class TestDriver {
                 + "ORDER BY continents.name, country.name, region.name, "
                 + "world_airports.municipality, world_airports.name ASC";
         testDriver = new Driver();
-        driver.setCount(match, filterQuery);
+        testDriver.setCount(match, filterQuery);
         assertEquals(testDriver.count, expected);
     }
-
-    @Test
-    public void testAddPlaces(){
-
-    }
-
-
-
 }
