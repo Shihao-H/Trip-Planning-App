4b864d82e61824320a8280a55995ee44e7825989
Shihao-H <4amlosangeles@gmail.com>
2018-11-16 03:07:33 -0700

    TestUtility test2optreverse

--- server/src/test/java/com/tripco/t03/planner/TestUtility.java
+++ server/src/test/java/com/tripco/t03/planner/TestUtility.java
@@ -5,6 +5,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class TestUtility {
+    private Utility Uti;
     private int[] arr;
     private long[][] grid;
 
@@ -18,25 +19,11 @@ public class TestUtility {
     }
 
     @Test
-    public void testopt2Reverse(){
-        opt = new ThreeOpt(arr, grid);
-        Assert.assertNotNull(opt);
+    public void testConstructor(){
+        Uti = new Utility();
+        Assert.assertNotNull(Uti);
     }
 
-    @Test
-    public void testReverse(){
-        opt = new ThreeOpt(arr, grid);
-        int [] arr3=opt.ReverseArray(opt.index);
-        int[] arr2 = new int[]{3, 2, 1, 0};
-        Assert.assertArrayEquals(arr2, arr3);
-    }
 
-    @Test
-    public void testSection(){
-        opt = new ThreeOpt(arr, grid);
-        int [] arr4=opt.Section(0,2,opt.index);
-        int [] arr5=new int[]{0,1,2};
-        Assert.assertArrayEquals(arr4, arr5);
-    }
 
 }
