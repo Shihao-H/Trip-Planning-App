f9d6d5acc3f11659d2477bab44fe1a0bece5b092
Shihao-H <4amlosangeles@gmail.com>
2018-11-15 18:58:34 -0700

    Reverse Array function

--- server/src/main/java/com/tripco/t03/planner/ThreeOpt.java
+++ server/src/main/java/com/tripco/t03/planner/ThreeOpt.java
@@ -10,6 +10,15 @@ public class ThreeOpt {
         this.disGrid=arr2;
     }
 
-
+    public int[] ReverseArray(int[] arr)
+    {
+        for(int i = 0; i < arr.length / 2; i++)
+        {
+            int temp = arr[i];
+            arr[i] = arr[arr.length - i - 1];
+            arr[arr.length - i - 1] = temp;
+        }
+        return arr;
+    }
 
 }
