c0e128c8f922c43f5d2c37c89f89e5368f3b4e9d
Shihao-H <4amlosangeles@gmail.com>
2018-11-15 18:57:53 -0700

    Build ThreeOpt java class and constructor

---
+++ server/src/main/java/com/tripco/t03/planner/ThreeOpt.java
@@ -0,0 +1,15 @@
+package com.tripco.t03.planner;
+
+public class ThreeOpt {
+    public int[] index;
+    public long[][] disGrid;
+
+    public ThreeOpt(int[] arr,long[][] arr2)
+    {
+        this.index=arr;
+        this.disGrid=arr2;
+    }
+
+
+
+}
