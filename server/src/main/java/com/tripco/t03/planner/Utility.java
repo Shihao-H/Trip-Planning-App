06eb3c183dc0572d09dabbe276de8834334b7b44
Shihao-H <4amlosangeles@gmail.com>
2018-11-16 02:42:47 -0700

    Build TestUtility

--- server/src/main/java/com/tripco/t03/planner/Utility.java
+++ server/src/main/java/com/tripco/t03/planner/Utility.java
@@ -22,6 +22,56 @@ public class Utility {
         return Dis;
     }
 
+    public boolean unvisitedCityLeft(boolean[] visit) {
+        boolean flag = false;
+        for (int i = 0; i < visit.length; i++) {
+            if (!visit[i])
+                return true;
+        }
+        return flag;
+    }
 
+    private int getMin(long[] numbers, boolean[] visit, long[] total, int k) {
+        long minValue = -1;
+        int i, index = -1;
 
+        for (i = 0; i < numbers.length; i++) {
+            if (!visit[i]) {
+                minValue = numbers[i];
+                index = i;
+                break;
+            }
+        }
+
+        while (i < numbers.length) {
+            if (numbers[i] < minValue) {
+                if (!visit[i]) {
+                    minValue = numbers[i];
+                    index = i;
+                }
+            }
+            i++;
+        }
+
+        visit[index] = true;
+        total[k] += minValue;
+        return index;
+    }
+
+    public int[] StartNear(int head, long[][] disGrid, int n) {
+        int count = 0;
+        int[] arr = new int[n];
+        arr[0] = head;
+        count++;
+        long total[] = new long[n];
+        boolean[] visit = new boolean[n];
+        visit[head] = true;
+        while (unvisitedCityLeft(visit)) {
+            int index = getMin(disGrid[head], visit, total, head);
+            head = index;
+            arr[count] = index;
+            count++;
+        }
+        return arr;
+    }
 }
