1d096d19ea17937e9b597d1fe84fdbba79468d1e
Lacey Willmann <lwillman@rams.colostate.edu>
2018-11-20 10:14:04 -0700

    Adding diagram of user interface

--- server/src/test/java/com/tripco/t03/server/TestDriver.java
+++ server/src/test/java/com/tripco/t03/server/TestDriver.java
@@ -1,6 +1,7 @@
 package com.tripco.t03.server;
 
 import com.tripco.t03.planner.Place;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -9,113 +10,123 @@ import java.util.ArrayList;
 import static org.junit.Assert.assertEquals;
 
 public class TestDriver {
-    private Driver testDriver;
     private String match;
     private int limit;
     private String filterQuery;
     private String limitQuery;
     private ArrayList<Place> expected;
+    private String filter;
     
     @Before
     public void setup(){
         match = "alf";
         filterQuery = "AND continents.name IN (\"North America\")\n"
-                      + "AND type IN (\"heliport\")\n";
-        limitQuery = "limit 20";
+                + "AND type IN (\"heliport\")\n";
+        
         limit = 20;
         expected = new ArrayList<>();
         expected.add(new Place("0FL4", "Jackson Heliport", 30.705, -85.378304));
         expected.add(new Place("98TX", "Cig 402 Heliport", 27.256701, -98.0942));
         expected.add(new Place("US-VA38", "Lz Alfa Heliport", 36.793201, -75.964699));
+        match = "death";
+        limit = 1;
+        filter = "closed";
     }
-    
+
     @Test
-    public void testFind() throws Exception{
-        testDriver = new Driver();
+    public void testFind(){
+        limitQuery = "";     //no limit
+        Driver.find(match, limit, filter);
+        ArrayList<Place> placeList = Driver.places;
+        
+        Assert.assertNotNull(placeList);
+    }
+
+    @Test
+    public void testFind1(){
+        limitQuery = "limit 20";
         int found = 3;
-        testDriver.find(match, limit, filterQuery);
-        assertEquals(expected.size(), testDriver.places.size());
-        assertEquals(found, testDriver.found);
-        assertEquals(limitQuery, testDriver.limitQuery);
-        if(testDriver.isTravis != null
-           && testDriver.isTravis.equals("true")) {
-            assertEquals("jdbc:mysql://127.0.0.1/cs314", testDriver.dburl);
-            assertEquals("travis", testDriver.username);
-        } else if(testDriver.isDevelopment != null
-                  && testDriver.isDevelopment.equals("development")) {
-            assertEquals("jdbc:mysql://127.0.0.1:some-port/cs314", testDriver.dburl);
-            assertEquals("cs314-db", testDriver.username);
+        Driver.find(match, limit, filterQuery);
+        assertEquals(expected.size(), Driver.places.size());
+        assertEquals(found, Driver.found);
+        assertEquals(limitQuery, Driver.limitQuery);
+        if(Driver.isTravis != null
+           && Driver.isTravis.equals("true")) {
+            assertEquals("jdbc:mysql://127.0.0.1/cs314", Driver.dburl);
+            assertEquals("travis", Driver.username);
+        } else if(Driver.isDevelopment != null
+                  && Driver.isDevelopment.equals("development")) {
+            assertEquals("jdbc:mysql://127.0.0.1:some-port/cs314", Driver.dburl);
+            assertEquals("cs314-db", Driver.username);
         } else {
-            assertEquals("jdbc:mysql://faure.cs.colostate.edu/cs314", testDriver.dburl);
-            assertEquals("cs314-db", testDriver.username);
+            assertEquals("jdbc:mysql://faure.cs.colostate.edu/cs314", Driver.dburl);
+            assertEquals("cs314-db", Driver.username);
         }
     }
-    
+
     @Test
-    public void testFindWithZeroLimit() throws Exception{
+    public void testFindWithZeroLimit(){
         limit = 0;
         limitQuery = "";
-        testDriver = new Driver();
         int found = 3;
-        testDriver.find(match, limit, filterQuery);
-        assertEquals(expected.size(),testDriver.places.size()) ;
-        assertEquals(found, testDriver.found);
-        assertEquals(limitQuery, testDriver.limitQuery);
+        Driver.find(match, limit, filterQuery);
+        assertEquals(expected.size(), Driver.places.size()) ;
+        assertEquals(found, Driver.found);
+        assertEquals(limitQuery, Driver.limitQuery);
     }
-    
+
     @Test
-    public void testFindException() throws Exception{
-        testDriver = new Driver();
+    public void testFindException(){
+        limitQuery = "limit 20";
         filterQuery = "AND continents IN (\"North America\")\n"
-                      + "AND type IN (\"heliport\")\n";
-        testDriver.find(match, limit, filterQuery);
+                + "AND type IN (\"heliport\")\n";
+        Driver.find(match, limit, filterQuery);
     }
-    
+
     @Test
     public void testSetSearch(){
+        limitQuery = "";     //no limit
         String expected = "SELECT world_airports.name, world_airports.municipality, region.name, "
-                          + "country.name, continents.name, "
-                          + "world_airports.id, world_airports.type, world_airports.longitude, "
-                          + "world_airports.latitude, "
-                          + "world_airports.elevation "
-                          + "FROM continents \n"
-                          + "INNER JOIN country ON continents.id = country.continent \n"
-                          + "INNER JOIN region ON country.id = region.iso_country \n"
-                          + "INNER JOIN world_airports ON region.id = world_airports.iso_region \n"
-                          + "WHERE (continents.name LIKE \"%" + match + "%\"  \n"
-                          + "OR country.name LIKE \"%" + match + "%\"  \n"
-                          + "OR region.name LIKE \"%" + match + "%\"  \n"
-                          + "OR world_airports.municipality LIKE \"%" + match + "%\" \n"
-                          + "OR world_airports.id LIKE \"%" + match + "%\" \n"
-                          + "OR world_airports.name LIKE \"%" + match + "%\") \n"
-                          + filterQuery
-                          + "ORDER BY continents.name, country.name, region.name, "
-                          + "world_airports.municipality, world_airports.name ASC "
-                          + limitQuery;
-        testDriver = new Driver();
-        testDriver.setLimit(limit);
-        testDriver.setSearch(match, filterQuery);
-        assertEquals(expected, testDriver.search);
+                + "country.name, continents.name, "
+                + "world_airports.id, world_airports.type, world_airports.longitude, "
+                + "world_airports.latitude, "
+                + "world_airports.elevation "
+                + "FROM continents \n"
+                + "INNER JOIN country ON continents.id = country.continent \n"
+                + "INNER JOIN region ON country.id = region.iso_country \n"
+                + "INNER JOIN world_airports ON region.id = world_airports.iso_region \n"
+                + "WHERE (continents.name LIKE \"%" + match + "%\"  \n"
+                + "OR country.name LIKE \"%" + match + "%\"  \n"
+                + "OR region.name LIKE \"%" + match + "%\"  \n"
+                + "OR world_airports.municipality LIKE \"%" + match + "%\" \n"
+                + "OR world_airports.id LIKE \"%" + match + "%\" \n"
+                + "OR world_airports.name LIKE \"%" + match + "%\") \n"
+                + filterQuery
+                + " ORDER BY continents.name, country.name, region.name, "
+                + "world_airports.municipality, world_airports.name ASC "
+                + limitQuery;
+        Driver.setSearch(match, filterQuery);
+        assertEquals(expected, Driver.search);
     }
-    
+
     @Test
     public void testSetCount(){
+        limitQuery = "";     //no limit
         String expected = "SELECT count(*) "
-                          + "FROM continents \n"
-                          + "INNER JOIN country ON continents.id = country.continent \n"
-                          + "INNER JOIN region ON country.id = region.iso_country \n"
-                          + "INNER JOIN world_airports ON region.id = world_airports.iso_region \n"
-                          + "WHERE (continents.name LIKE \"%" + match + "%\"  \n"
-                          + "OR country.name LIKE \"%" + match + "%\"  \n"
-                          + "OR region.name LIKE \"%" + match + "%\"  \n"
-                          + "OR world_airports.municipality LIKE \"%" + match + "%\" \n"
-                          + "OR world_airports.id LIKE \"%" + match + "%\" \n"
-                          + "OR world_airports.name LIKE \"%" + match + "%\") \n"
-                          + filterQuery
-                          + "ORDER BY continents.name, country.name, region.name, "
-                          + "world_airports.municipality, world_airports.name ASC";
-        testDriver = new Driver();
-        testDriver.setCount(match, filterQuery);
-        assertEquals(expected, testDriver.count);
+                + "FROM continents \n"
+                + "INNER JOIN country ON continents.id = country.continent \n"
+                + "INNER JOIN region ON country.id = region.iso_country \n"
+                + "INNER JOIN world_airports ON region.id = world_airports.iso_region \n"
+                + "WHERE (continents.name LIKE \"%" + match + "%\"  \n"
+                + "OR country.name LIKE \"%" + match + "%\"  \n"
+                + "OR region.name LIKE \"%" + match + "%\"  \n"
+                + "OR world_airports.municipality LIKE \"%" + match + "%\" \n"
+                + "OR world_airports.id LIKE \"%" + match + "%\" \n"
+                + "OR world_airports.name LIKE \"%" + match + "%\") \n"
+                + filterQuery
+                + "ORDER BY continents.name, country.name, region.name, "
+                + "world_airports.municipality, world_airports.name ASC";
+        Driver.setCount(match, filterQuery);
+        assertEquals(expected, Driver.count);
     }
 }
