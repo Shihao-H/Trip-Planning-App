2a2d77b21464f4ebe797fd2114ab8e2496484e59
Lacey Willmann <lwillman@rams.colostate.edu>
2018-11-20 17:30:21 -0700

    Fixed some complexity issues with Driver.java and TestDriver.

--- server/src/test/java/com/tripco/t03/server/TestDriver.java
+++ server/src/test/java/com/tripco/t03/server/TestDriver.java
@@ -15,12 +15,12 @@ public class TestDriver {
     private String filterQuery;
     private String limitQuery;
     private ArrayList<Place> expected;
-
+    
     @Before
     public void setup(){
         match = "alf";
         filterQuery = "AND continents.name IN (\"North America\")\n"
-                + "AND type IN (\"heliport\")\n";
+                      + "AND type IN (\"heliport\")\n";
         limitQuery = "limit 20";
         limit = 20;
         expected = new ArrayList<>();
@@ -28,7 +28,7 @@ public class TestDriver {
         expected.add(new Place("98TX", "Cig 402 Heliport", 27.256701, -98.0942));
         expected.add(new Place("US-VA38", "Lz Alfa Heliport", 36.793201, -75.964699));
     }
-
+    
     @Test
     public void testFind(){
         testDriver = new Driver();
@@ -38,11 +38,11 @@ public class TestDriver {
         assertEquals(found, testDriver.found);
         assertEquals(limitQuery, testDriver.limitQuery);
         if(testDriver.isTravis != null
-                && testDriver.isTravis.equals("true")) {
+           && testDriver.isTravis.equals("true")) {
             assertEquals("jdbc:mysql://127.0.0.1/cs314", testDriver.dburl);
             assertEquals("travis", testDriver.username);
         } else if(testDriver.isDevelopment != null
-                && testDriver.isDevelopment.equals("development")) {
+                  && testDriver.isDevelopment.equals("development")) {
             assertEquals("jdbc:mysql://127.0.0.1:some-port/cs314", testDriver.dburl);
             assertEquals("cs314-db", testDriver.username);
         } else {
@@ -50,7 +50,7 @@ public class TestDriver {
             assertEquals("cs314-db", testDriver.username);
         }
     }
-
+    
     @Test
     public void testFindWithZeroLimit(){
         limit = 0;
@@ -62,58 +62,58 @@ public class TestDriver {
         assertEquals(found, testDriver.found);
         assertEquals(limitQuery, testDriver.limitQuery);
     }
-
+    
     @Test
     public void testFindException(){
         testDriver = new Driver();
-        boolean thrown = false;
         filterQuery = "AND continents IN (\"North America\")\n"
-                + "AND type IN (\"heliport\")\n";
+                      + "AND type IN (\"heliport\")\n";
         testDriver.find(match, limit, filterQuery);
     }
-
+    
     @Test
     public void testSetSearch(){
         String expected = "SELECT world_airports.name, world_airports.municipality, region.name, "
-                + "country.name, continents.name, "
-                + "world_airports.id, world_airports.type, world_airports.longitude, "
-                + "world_airports.latitude, "
-                + "world_airports.elevation "
-                + "FROM continents \n"
-                + "INNER JOIN country ON continents.id = country.continent \n"
-                + "INNER JOIN region ON country.id = region.iso_country \n"
-                + "INNER JOIN world_airports ON region.id = world_airports.iso_region \n"
-                + "WHERE (continents.name LIKE \"%" + match + "%\"  \n"
-                + "OR country.name LIKE \"%" + match + "%\"  \n"
-                + "OR region.name LIKE \"%" + match + "%\"  \n"
-                + "OR world_airports.municipality LIKE \"%" + match + "%\" \n"
-                + "OR world_airports.id LIKE \"%" + match + "%\" \n"
-                + "OR world_airports.name LIKE \"%" + match + "%\") \n"
-                + filterQuery
-                + "ORDER BY continents.name, country.name, region.name, "
-                + "world_airports.municipality, world_airports.name ASC "
-                + limitQuery;
+                          + "country.name, continents.name, "
+                          + "world_airports.id, world_airports.type, world_airports.longitude, "
+                          + "world_airports.latitude, "
+                          + "world_airports.elevation "
+                          + "FROM continents \n"
+                          + "INNER JOIN country ON continents.id = country.continent \n"
+                          + "INNER JOIN region ON country.id = region.iso_country \n"
+                          + "INNER JOIN world_airports ON region.id = world_airports.iso_region \n"
+                          + "WHERE (continents.name LIKE \"%" + match + "%\"  \n"
+                          + "OR country.name LIKE \"%" + match + "%\"  \n"
+                          + "OR region.name LIKE \"%" + match + "%\"  \n"
+                          + "OR world_airports.municipality LIKE \"%" + match + "%\" \n"
+                          + "OR world_airports.id LIKE \"%" + match + "%\" \n"
+                          + "OR world_airports.name LIKE \"%" + match + "%\") \n"
+                          + filterQuery
+                          + "ORDER BY continents.name, country.name, region.name, "
+                          + "world_airports.municipality, world_airports.name ASC "
+                          + limitQuery;
         testDriver = new Driver();
+        testDriver.setLimit(limit);
         testDriver.setSearch(match, filterQuery);
         assertEquals(expected, testDriver.search);
     }
-
+    
     @Test
     public void testSetCount(){
         String expected = "SELECT count(*) "
-                + "FROM continents \n"
-                + "INNER JOIN country ON continents.id = country.continent \n"
-                + "INNER JOIN region ON country.id = region.iso_country \n"
-                + "INNER JOIN world_airports ON region.id = world_airports.iso_region \n"
-                + "WHERE (continents.name LIKE \"%" + match + "%\"  \n"
-                + "OR country.name LIKE \"%" + match + "%\"  \n"
-                + "OR region.name LIKE \"%" + match + "%\"  \n"
-                + "OR world_airports.municipality LIKE \"%" + match + "%\" \n"
-                + "OR world_airports.id LIKE \"%" + match + "%\" \n"
-                + "OR world_airports.name LIKE \"%" + match + "%\") \n"
-                + filterQuery
-                + "ORDER BY continents.name, country.name, region.name, "
-                + "world_airports.municipality, world_airports.name ASC";
+                          + "FROM continents \n"
+                          + "INNER JOIN country ON continents.id = country.continent \n"
+                          + "INNER JOIN region ON country.id = region.iso_country \n"
+                          + "INNER JOIN world_airports ON region.id = world_airports.iso_region \n"
+                          + "WHERE (continents.name LIKE \"%" + match + "%\"  \n"
+                          + "OR country.name LIKE \"%" + match + "%\"  \n"
+                          + "OR region.name LIKE \"%" + match + "%\"  \n"
+                          + "OR world_airports.municipality LIKE \"%" + match + "%\" \n"
+                          + "OR world_airports.id LIKE \"%" + match + "%\" \n"
+                          + "OR world_airports.name LIKE \"%" + match + "%\") \n"
+                          + filterQuery
+                          + "ORDER BY continents.name, country.name, region.name, "
+                          + "world_airports.municipality, world_airports.name ASC";
         testDriver = new Driver();
         testDriver.setCount(match, filterQuery);
         assertEquals(expected, testDriver.count);
