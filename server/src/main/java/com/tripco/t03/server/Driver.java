6d6b8a3756f6ed919a75a626b21d0ea8cea8dcdc
Lacey Willmann <lwillman@rams.colostate.edu>
2018-11-21 22:15:47 -0700

    Cleaned up NearestNeighbor.java Reduced complexity

--- server/src/main/java/com/tripco/t03/server/Driver.java
+++ server/src/main/java/com/tripco/t03/server/Driver.java
@@ -2,114 +2,170 @@ package com.tripco.t03.server;
 
 import com.tripco.t03.planner.Place;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 public class Driver {
     // db configuration information
-    public static String isTravis = System.getenv("TRAVIS");
-    public static String isDevelopment = System.getenv("CS314_ENV");
-    public static String dburl;
-    public static String username;
-    public static String password;
-    private static final String myDriver = "com.mysql.jdbc.Driver";
+    String isTravis;
+    String isDevelopment;
+    String dburl;
+    String username;
+    public String password;
     // fill in SQL queries to count the number of records and to retrieve the data
-    public static String count = "";
-    public static String search = "";
-    public static String limitQuery = "";
-    public static ArrayList<Place> places;
-    public static int found = 0;
-
+    public String count;
+    public String search;
+    String limitQuery;
+    public ArrayList<Place> places;
+    public int found;
+    
+    /**
+     * Constructor.
+     */
+    public Driver() {
+        this.limitQuery = "";
+        this.search = "";
+        this.count = "";
+        this.found = 0;
+        this.isTravis = System.getenv("TRAVIS");
+        this.isDevelopment = System.getenv("CS314_ENV");
+    }
+    
+    /**
+     * Setter.
+     */
+    void setDburlUserName(){
+        if(this.isTravis != null && this.isTravis.equals("true")) {
+            this.dburl = "jdbc:mysql://127.0.0.1/cs314";
+            this.username = "travis";
+        } else if(this.isDevelopment != null && this.isDevelopment.equals("development")) {
+            this.dburl = "jdbc:mysql://127.0.0.1:8098/cs314";
+            this.username = "cs314-db";
+        } else {
+            this.dburl = "jdbc:mysql://faure.cs.colostate.edu/cs314";
+            this.username = "cs314-db";
+        }
+    }
+    
+    /**
+     * Setter.
+     */
+    void setPassword() {
+        if(this.isTravis != null && this.isTravis.equals("true")) {
+            this.password = null;
+        } else {
+            this.password = "eiK5liet1uej";
+        }
+    }
+    
     /**
      * The find method is meant to get access to the database and execute queries.
      * @param match String phrase to match.
      * @param limit integer number of mx results to be shown.
      */
-    public static void find(String match, int limit, String filter) {
-        if(isTravis != null && isTravis.equals("true")) {
-            dburl = "jdbc:mysql://127.0.0.1/cs314";
-            username = "travis";
-            password = null;
-        } else if(isDevelopment != null && isDevelopment.equals("development")) {
-            dburl = "jdbc:mysql://127.0.0.1:some-port/cs314";
-            username = "cs314-db";
-            password = "eiK5liet1uej";
-        } else {
-            dburl = "jdbc:mysql://faure.cs.colostate.edu/cs314";
-            username = "cs314-db";
-            password =  "eiK5liet1uej";
-        }
-
-        if (limit == 0) {
-            limitQuery = ""; // no limit
-        } else {
-            limitQuery = "limit " + Integer.toString(limit);
-        }
+    public void find(String match, int limit, String filter) throws SQLException,
+                                                                    ClassNotFoundException {
+        setDburlUserName();
+        setPassword();
+        setLimit(limit);
         setSearch(match, filter);
         setCount(match, filter);
-        try {
-            Class.forName(myDriver);
-            try (Connection conn = DriverManager.getConnection(dburl, username, password);
-                 Statement stCount = conn.createStatement();
-                 Statement stQuery = conn.createStatement();
-                 ResultSet rsCount = stCount.executeQuery(count);
-                 ResultSet rsQuery = stQuery.executeQuery(search)) {
-                 rsCount.next();
-                 found = rsCount.getInt(1);
-                 places = new ArrayList<>();
-                 while (rsQuery.next()) {
-                    final Place place = new Place(
-                            rsQuery.getString("id"),
-                            rsQuery.getString(1),//name
-                            Double.parseDouble(rsQuery.getString("latitude")),
-                            Double.parseDouble(rsQuery.getString("longitude")));
-                    place.setAttributeType(rsQuery.getString("type"));
-                    place.setAttributeElevation(rsQuery.getString("elevation"));
-                    place.setAttributeContinent(rsQuery.getString(5));//continent
-                    place.setAttributeCountry(rsQuery.getString(4));//country
-                    place.setAttributeRegion(rsQuery.getString(3));//region
-                    place.setAttributeMunicipality(rsQuery.getString("municipality"));
-                    places.add(place);
-                }
-            }
-        } catch (Exception e) {
-            System.err.println("Exception: " + e.getMessage());
+        ResultSet rsCount = runQuery(count);
+        rsCount.next();
+        this.found = rsCount.getInt(1);
+        this.places = new ArrayList<>();
+        ResultSet rsQuery = runQuery(search);
+        parseQuery(rsQuery);
+    }
+    
+    /**
+     * Helper method to parse query results.
+     * @param query ResultSet.
+     * @throws SQLException upon failure.
+     */
+    private void parseQuery(ResultSet query) throws SQLException {
+        if (query.next()) {
+            final Place place = new Place(
+                    query.getString("id"),
+                    query.getString(1),//name
+                    Double.parseDouble(query.getString("latitude")),
+                    Double.parseDouble(query.getString("longitude")));
+            place.setAttributeType(query.getString("type"));
+            place.setAttributeElevation(query.getString("elevation"));
+            place.setAttributeContinent(query.getString(5));//continent
+            place.setAttributeCountry(query.getString(4));//country
+            place.setAttributeRegion(query.getString(3));//region
+            place.setAttributeMunicipality(query.getString("municipality"));
+            this.places.add(place);
+            parseQuery(query);
         }
     }
-
+    
+    /**
+     * Helper method to run query.
+     * @param query String.
+     * @return ResultSet.
+     * @throws SQLException upon failure.
+     * @throws ClassNotFoundException upon failure.
+     */
+    private ResultSet runQuery(String query) throws SQLException, ClassNotFoundException {
+        String myDriver = "com.mysql.jdbc.Driver";
+        Class.forName(myDriver);
+        Connection conn = DriverManager.getConnection(this.dburl, this.username,
+                                                      this.password);
+        Statement statement = conn.createStatement();
+        return statement.executeQuery(query);
+    }
+    
+    /**
+     * Setter for search.
+     * @param limit int.
+     */
+    void setLimit(int limit){
+        if (limit == 0) {
+            this.limitQuery = ""; // no limit
+        } else {
+            this.limitQuery = "limit " + Integer.toString(limit);
+        }
+    }
+    
     /**
      * Setter for search.
      * @param match String.
      * @param filter String.
      */
-    public static void setSearch(String match, String filter) {
-        search = "SELECT world_airports.name, world_airports.municipality, region.name, "
-                + "country.name, continents.name, "
-                + "world_airports.id, world_airports.type, world_airports.longitude, "
-                + "world_airports.latitude, "
-                + "world_airports.elevation "
-                + "FROM continents \n"
-                + "INNER JOIN country ON continents.id = country.continent \n"
-                + "INNER JOIN region ON country.id = region.iso_country \n"
-                + "INNER JOIN world_airports ON region.id = world_airports.iso_region \n"
-                + "WHERE (continents.name LIKE \"%" + match + "%\"  \n"
-                + "OR country.name LIKE \"%" + match + "%\"  \n"
-                + "OR region.name LIKE \"%" + match + "%\"  \n"
-                + "OR world_airports.municipality LIKE \"%" + match + "%\" \n"
-                + "OR world_airports.id LIKE \"%" + match + "%\" \n"
-                + "OR world_airports.name LIKE \"%" + match + "%\") \n"
-                + filter
-                + "ORDER BY continents.name, country.name, region.name, "
-                + "world_airports.municipality, world_airports.name ASC "
-                + limitQuery;
+    void setSearch(String match, String filter) {
+        this.search = "SELECT world_airports.name, world_airports.municipality, region.name, "
+                      + "country.name, continents.name, "
+                      + "world_airports.id, world_airports.type, world_airports.longitude, "
+                      + "world_airports.latitude, "
+                      + "world_airports.elevation "
+                      + "FROM continents \n"
+                      + "INNER JOIN country ON continents.id = country.continent \n"
+                      + "INNER JOIN region ON country.id = region.iso_country \n"
+                      + "INNER JOIN world_airports ON region.id = world_airports.iso_region \n"
+                      + "WHERE (continents.name LIKE \"%" + match + "%\"  \n"
+                      + "OR country.name LIKE \"%" + match + "%\"  \n"
+                      + "OR region.name LIKE \"%" + match + "%\"  \n"
+                      + "OR world_airports.municipality LIKE \"%" + match + "%\" \n"
+                      + "OR world_airports.id LIKE \"%" + match + "%\" \n"
+                      + "OR world_airports.name LIKE \"%" + match + "%\") \n"
+                      + filter
+                      + "ORDER BY continents.name, country.name, region.name, "
+                      + "world_airports.municipality, world_airports.name ASC "
+                      + this.limitQuery;
     }
-
+    
     /**
      * Setter: Sets count variable.
      * @param match String.
      * @param filter String.
      */
-    public static void setCount(String match, String filter) {
+    void setCount(String match, String filter) {
         count = "SELECT count(*) "
                 + "FROM continents \n"
                 + "INNER JOIN country ON continents.id = country.continent \n"
--- server/src/test/java/com/tripco/t03/server/TestDriver.java
+++ server/src/test/java/com/tripco/t03/server/TestDriver.java
@@ -15,12 +15,12 @@ public class TestDriver {
     private String filterQuery;
     private String limitQuery;
     private ArrayList<Place> expected;
-
+    
     @Before
     public void setup(){
         match = "alf";
         filterQuery = "AND continents.name IN (\"North America\")\n"
-                + "AND type IN (\"heliport\")\n";
+                      + "AND type IN (\"heliport\")\n";
         limitQuery = "limit 20";
         limit = 20;
         expected = new ArrayList<>();
@@ -66,56 +66,74 @@ public class TestDriver {
     @Test
     public void testFindException(){
         testDriver = new Driver();
-        boolean thrown = false;
-        filterQuery = "AND continents IN (\"North America\")\n"
-                + "AND type IN (\"heliport\")\n";
-        testDriver.find(match, limit, filterQuery);
+        String except = "";
+        try{
+            filterQuery = "AND continents IN (\"North America\")\n"
+                          + "AND type IN (\"heliport\")\n";
+            testDriver.find(match, limit, filterQuery);
+            } catch(Exception e){
+                except = e.toString();
+            }
+            String expected = "com.mysql.jdbc.exceptions.jdbc4.MySQLSyntaxErrorException: "
+                              + "Unknown column \'continents\' in \'where clause\'";
+            assertEquals(except, expected);
     }
-
+    
     @Test
     public void testSetSearch(){
         String expected = "SELECT world_airports.name, world_airports.municipality, region.name, "
-                + "country.name, continents.name, "
-                + "world_airports.id, world_airports.type, world_airports.longitude, "
-                + "world_airports.latitude, "
-                + "world_airports.elevation "
-                + "FROM continents \n"
-                + "INNER JOIN country ON continents.id = country.continent \n"
-                + "INNER JOIN region ON country.id = region.iso_country \n"
-                + "INNER JOIN world_airports ON region.id = world_airports.iso_region \n"
-                + "WHERE (continents.name LIKE \"%" + match + "%\"  \n"
-                + "OR country.name LIKE \"%" + match + "%\"  \n"
-                + "OR region.name LIKE \"%" + match + "%\"  \n"
-                + "OR world_airports.municipality LIKE \"%" + match + "%\" \n"
-                + "OR world_airports.id LIKE \"%" + match + "%\" \n"
-                + "OR world_airports.name LIKE \"%" + match + "%\") \n"
-                + filterQuery
-                + "ORDER BY continents.name, country.name, region.name, "
-                + "world_airports.municipality, world_airports.name ASC "
-                + limitQuery;
+                          + "country.name, continents.name, "
+                          + "world_airports.id, world_airports.type, world_airports.longitude, "
+                          + "world_airports.latitude, "
+                          + "world_airports.elevation "
+                          + "FROM continents \n"
+                          + "INNER JOIN country ON continents.id = country.continent \n"
+                          + "INNER JOIN region ON country.id = region.iso_country \n"
+                          + "INNER JOIN world_airports ON region.id = world_airports.iso_region \n"
+                          + "WHERE (continents.name LIKE \"%" + match + "%\"  \n"
+                          + "OR country.name LIKE \"%" + match + "%\"  \n"
+                          + "OR region.name LIKE \"%" + match + "%\"  \n"
+                          + "OR world_airports.municipality LIKE \"%" + match + "%\" \n"
+                          + "OR world_airports.id LIKE \"%" + match + "%\" \n"
+                          + "OR world_airports.name LIKE \"%" + match + "%\") \n"
+                          + filterQuery
+                          + "ORDER BY continents.name, country.name, region.name, "
+                          + "world_airports.municipality, world_airports.name ASC "
+                          + limitQuery;
         testDriver = new Driver();
+        testDriver.setLimit(limit);
         testDriver.setSearch(match, filterQuery);
+
         assertEquals(expected, testDriver.search);
     }
-
+    
     @Test
     public void testSetCount(){
         String expected = "SELECT count(*) "
-                + "FROM continents \n"
-                + "INNER JOIN country ON continents.id = country.continent \n"
-                + "INNER JOIN region ON country.id = region.iso_country \n"
-                + "INNER JOIN world_airports ON region.id = world_airports.iso_region \n"
-                + "WHERE (continents.name LIKE \"%" + match + "%\"  \n"
-                + "OR country.name LIKE \"%" + match + "%\"  \n"
-                + "OR region.name LIKE \"%" + match + "%\"  \n"
-                + "OR world_airports.municipality LIKE \"%" + match + "%\" \n"
-                + "OR world_airports.id LIKE \"%" + match + "%\" \n"
-                + "OR world_airports.name LIKE \"%" + match + "%\") \n"
-                + filterQuery
-                + "ORDER BY continents.name, country.name, region.name, "
-                + "world_airports.municipality, world_airports.name ASC";
+                          + "FROM continents \n"
+                          + "INNER JOIN country ON continents.id = country.continent \n"
+                          + "INNER JOIN region ON country.id = region.iso_country \n"
+                          + "INNER JOIN world_airports ON region.id = world_airports.iso_region \n"
+                          + "WHERE (continents.name LIKE \"%" + match + "%\"  \n"
+                          + "OR country.name LIKE \"%" + match + "%\"  \n"
+                          + "OR region.name LIKE \"%" + match + "%\"  \n"
+                          + "OR world_airports.municipality LIKE \"%" + match + "%\" \n"
+                          + "OR world_airports.id LIKE \"%" + match + "%\" \n"
+                          + "OR world_airports.name LIKE \"%" + match + "%\") \n"
+                          + filterQuery
+                          + "ORDER BY continents.name, country.name, region.name, "
+                          + "world_airports.municipality, world_airports.name ASC";
         testDriver = new Driver();
         testDriver.setCount(match, filterQuery);
         assertEquals(expected, testDriver.count);
     }
+    
+    @Test
+    public void testSetLimit(){
+        testDriver = new Driver();
+        testDriver.setLimit(3);
+        
+        assertEquals(testDriver.limitQuery, "limit 3");
+    }
+    
 }
