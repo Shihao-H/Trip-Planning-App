c4b308775de5140820e054a2808c4eaecf6c9ff6
MinjieSh <42553263+minjiesh@users.noreply.github.com>
2018-11-27 17:59:39 -0700

    * reduce duplicates in options

--- client/src/components/Application/OptionPanel.js
+++ client/src/components/Application/OptionPanel.js
@@ -10,18 +10,18 @@ class OptionPanel extends Component {
         super(props);
         this.state = {ifDisplayUserDefinedInputFields: false, collapse: false,};
     }
-
+//{let optionValue = ((optionName == "optimization") ? optionObject.label : optionObject);
     generateOptionsButtons(type,optionName){
-        const buttons = this.props.config[type].map((optionValue) =>
-            <Button key={'options_button_' + type}
+        return this.props.config[type].map((optionValue) =>
+
+            <Button key={'options_button_' + optionValue}
                     className='btn-outline-dark options-button'
                     active={this.props.options[optionName] === optionValue}
                     value={optionValue}
                     onClick={(event) => this.clickButton(event, optionName)}>
                 {optionValue.charAt(0).toUpperCase() + optionValue.slice(1)}
-            </Button>
+                </Button>
         );
-        return buttons;
     }
 
     clickButton(event,optionName) {
@@ -37,8 +37,7 @@ class OptionPanel extends Component {
     {
         return(
             <FormGroup>
-                <br/>
-                <Label>Unit Name:</Label>
+                <br/><Label>Unit Name:</Label>
                 <Input type="text" placeholder="Enter unit name" onChange={event => {
                     this.props.updateOptions('unitName', event.target.value)
                 }}/>
@@ -51,28 +50,29 @@ class OptionPanel extends Component {
         )
     }
 
-    render(){
-        <div>
-            <Card>
-                <CardBody>
-                    <ButtonGroup vertical> {this.generateOptionsButtons("units","units")} </ButtonGroup>
-                    {this.state.ifDisplayUserDefinedInputFields && (
-                        <Form>
-                            {this.renderUserDefinedForm()}
-                        </Form>)}
-                    <ButtonGroup vertical> {this.generateOptionsButtons("optimization","optimization")} </ButtonGroup>
-                    <ButtonGroup vertical> {this.generateOptionsButtons("maps","mapForOption")} </ButtonGroup>
-                    <Attributes config={this.props.config} display={this.props.display}
-                                updateAttributes={this.props.updateAttributes}/>
-                    <InterOperate host={this.props.host} otherTeams={this.props.otherTeams}
-                                  updateHost={this.props.updateHost}
-                                  updateInterOperate={this.props.updateInterOperate}
-                                  updateOtherTeams={this.props.updateOtherTeams}/>
-                </CardBody>
-            </Card>
-        </div>
-    }
+    render() {
+        return (
+            <div>
+                <Card>
+                    <CardBody>
+                        <ButtonGroup vertical>{this.generateOptionsButtons("units", "units")}</ButtonGroup><br/>
+                        {this.state.ifDisplayUserDefinedInputFields && (
+                            <Form>
+                                {this.renderUserDefinedForm()}
+                            </Form>)}<br/>
 
+                        <ButtonGroup vertical>{this.generateOptionsButtons("maps", "mapForOption")}</ButtonGroup><br/>
+                        <Attributes config={this.props.config} display={this.props.display}
+                                    updateAttributes={this.props.updateAttributes}/>
+                        <InterOperate host={this.props.host} otherTeams={this.props.otherTeams}
+                                      updateHost={this.props.updateHost}
+                                      updateInterOperate={this.props.updateInterOperate}
+                                      updateOtherTeams={this.props.updateOtherTeams}/>
+                    </CardBody>
+                </Card>
+            </div>
+        );
+    }
 }
 
 export default OptionPanel;
