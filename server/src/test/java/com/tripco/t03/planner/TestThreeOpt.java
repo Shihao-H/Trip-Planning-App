04a80c7a97234f4c6742008781e6bf5b8f2ee057
Shihao-H <4amlosangeles@gmail.com>
2018-11-29 18:44:05 -0700

    update TestOpt3Case2

--- server/src/test/java/com/tripco/t03/planner/TestThreeOpt.java
+++ server/src/test/java/com/tripco/t03/planner/TestThreeOpt.java
@@ -76,7 +76,7 @@ public class TestThreeOpt {
         opt = new ThreeOpt(arr, grid);
         int [] arr6=new int[]{1,2,3,4,5,6};
         int [] arr7=new int[]{1,4,5,3,2,6};
-        opt.opt3Case1(0,2,4, arr6);
+        opt.opt3Case2(0,2,4, arr6);
         Assert.assertArrayEquals(arr6, arr7);
     }
 }
